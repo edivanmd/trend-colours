adc1d04fc032cf38bb645d573f4d1fa5845c760d
edivanmd <edivanmd@gmail.com>
2022-11-21 17:11:42 +1100

    Set Up

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
+import Head from 'next/head'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>Trend Colors</title>
+        <meta name="description" content="" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
