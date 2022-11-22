efc41e826786ac4acd2c9821669f7d428e087558
edivanmd <edivanmd@gmail.com>
2022-11-22 16:18:31 +1100

    CardColor component

---
+++ components/CardColor.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image from 'next/image'
+
+
+const CardColor = ({ infoCard }) => {
+  return (
+    <li className={'card bg-black ' + infoCard.classes }>
+      <h2>{infoCard.title}</h2>
+      {infoCard.image != 'none' && 
+        <Image src={infoCard.image} 
+          priority
+          alt='Image' 
+          width='200'
+          height='200'
+        />
+      }
+    </li>
+  )
+}
+
+export default CardColor
